2dd5626f9e497ef3797c7b79bf01bff4695de1f6
Palak <agarwal.palak.03@gmail.com>
2018-04-01 10:29:35 +0530

    change

--- app/models/note.rb
+++ app/models/note.rb
@@ -23,30 +23,10 @@ class Note
   	note = Note.find_by(id: self.id)
   	note.tags.destroy_all
   	hashtags = note.content.scan(/#\w+/)
-    hashtags.uniq.map do |hashtag| 
+    	hashtags.uniq.map do |hashtag| 
   		tag = note.tags.find_or_create_by(name: hashtag.downcase.delete('#'))
   		tag.save!
   	end
   end
 
-
-
-  # def self.tagged_with(name)
-  # 	Tag.find_by_name!(name).notes
-  # end
-
-  # def self.tag_counts
-  # 	Tag.select("tags.*, count(notes_tags.tag_id) as count").
-  # 	 joins(:notes_tags).group("notes_tags.tag_id")
-  # end
-
-  # def tag_list
-  # 	tags.map(&:name).join(", ")
-  # end
-
-  # def tag_list=(names)
-  # 	self.tags = names.split(",").map do |n|
-  # 		Tag.where(name: n.strip).first_or_create!
-  # 	end
-  # end
 end
